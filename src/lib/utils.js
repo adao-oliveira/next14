5af6d3ebe40582162b2b9d1773ffac7e1f33abf6
Adao Domingos <adaooliveira44@gmail.com>
2024-06-07 18:28:31 -0300

    ajustes string

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,17 +1,23 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const connection = {};
+const connection = {
+  isConnected: false,
+};
 
 export const connectToDb = async () => {
   try {
-    if(connection.isConnected) {
+    if (connection.isConnected) {
       console.log("Using existing connection");
       return;
     }
-    const db = await mongoose.connect(process.env.MONGO);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     connection.isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected");
   } catch (error) {
-    console.log(error);
+    console.error("MongoDB connection error:", error);
     throw new Error(error);
   }
 };
